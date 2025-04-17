bf3eb2593e4dc19d2ffba8911293d88d8d1a9b8d
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-17 22:37:07 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,10 @@
 export const data = [
     {
        "image": {
-            "thumbnail": "./public/assets/image-waffle-thumbnail.jpg",
-            "mobile": "./public/assets/image-waffle-mobile.jpg",
-            "tablet": "./public/assets/image-waffle-tablet.jpg",
-            "desktop": "./public/assets/image-waffle-desktop.jpg"
+            "thumbnail": "./assets/image-waffle-thumbnail.jpg",
+            "mobile": "./assets/image-waffle-mobile.jpg",
+            "tablet": "./assets/image-waffle-tablet.jpg",
+            "desktop": "./assets/image-waffle-desktop.jpg"
        },
        "name": "Waffle with Berries",
        "category": "Waffle",
@@ -12,10 +12,10 @@ export const data = [
     },
     {
         "image": {
-            "thumbnail": "./public/assets/image-creme-brulee-thumbnail.jpg",
-            "mobile": "./public/assets/image-creme-brulee-mobile.jpg",
-            "tablet": "./public/assets/image-creme-brulee-tablet.jpg",
-            "desktop": "./public/assets/image-creme-brulee-desktop.jpg"
+            "thumbnail": "./assets/image-creme-brulee-thumbnail.jpg",
+            "mobile": "./assets/image-creme-brulee-mobile.jpg",
+            "tablet": "./assets/image-creme-brulee-tablet.jpg",
+            "desktop": "./assets/image-creme-brulee-desktop.jpg"
         },
         "name": "Vanilla Bean Crème Brûlée",
         "category": "Crème Brûlée",
@@ -23,10 +23,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-macaron-thumbnail.jpg",
-            "mobile": "./public/assets/image-macaron-mobile.jpg",
-            "tablet": "./public/assets/image-macaron-tablet.jpg",
-            "desktop": "./public/assets/image-macaron-desktop.jpg"
+            "thumbnail": "./assets/image-macaron-thumbnail.jpg",
+            "mobile": "./assets/image-macaron-mobile.jpg",
+            "tablet": "./assets/image-macaron-tablet.jpg",
+            "desktop": "./assets/image-macaron-desktop.jpg"
         },
         "name": "Macaron Mix of Five",
         "category": "Macaron",
@@ -34,10 +34,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-tiramisu-thumbnail.jpg",
-            "mobile": "./public/assets/image-tiramisu-mobile.jpg",
-            "tablet": "./public/assets/image-tiramisu-tablet.jpg",
-            "desktop": "./public/assets/image-tiramisu-desktop.jpg"
+            "thumbnail": "./assets/image-tiramisu-thumbnail.jpg",
+            "mobile": "./assets/image-tiramisu-mobile.jpg",
+            "tablet": "./assets/image-tiramisu-tablet.jpg",
+            "desktop": "./assets/image-tiramisu-desktop.jpg"
         },
         "name": "Classic Tiramisu",
         "category": "Tiramisu",
@@ -45,10 +45,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-baklava-thumbnail.jpg",
-            "mobile": "./public/assets/image-baklava-mobile.jpg",
-            "tablet": "./public/assets/image-baklava-tablet.jpg",
-            "desktop": "./public/assets/image-baklava-desktop.jpg"
+            "thumbnail": "./assets/image-baklava-thumbnail.jpg",
+            "mobile": "./assets/image-baklava-mobile.jpg",
+            "tablet": "./assets/image-baklava-tablet.jpg",
+            "desktop": "./assets/image-baklava-desktop.jpg"
         },
         "name": "Pistachio Baklava",
         "category": "Baklava",
@@ -56,10 +56,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-meringue-thumbnail.jpg",
-            "mobile": "./public/assets/image-meringue-mobile.jpg",
-            "tablet": "./public/assets/image-meringue-tablet.jpg",
-            "desktop": "./public/assets/image-meringue-desktop.jpg"
+            "thumbnail": "./assets/image-meringue-thumbnail.jpg",
+            "mobile": "./assets/image-meringue-mobile.jpg",
+            "tablet": "./assets/image-meringue-tablet.jpg",
+            "desktop": "./assets/image-meringue-desktop.jpg"
         },
         "name": "Lemon Meringue Pie",
         "category": "Pie",
@@ -67,10 +67,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-cake-thumbnail.jpg",
-            "mobile": "./public/assets/image-cake-mobile.jpg",
-            "tablet": "./public/assets/image-cake-tablet.jpg",
-            "desktop": "./public/assets/image-cake-desktop.jpg"
+            "thumbnail": "./assets/image-cake-thumbnail.jpg",
+            "mobile": "./assets/image-cake-mobile.jpg",
+            "tablet": "./assets/image-cake-tablet.jpg",
+            "desktop": "./assets/image-cake-desktop.jpg"
         },
         "name": "Red Velvet Cake",
         "category": "Cake",
@@ -78,10 +78,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-brownie-thumbnail.jpg",
-            "mobile": "./public/assets/image-brownie-mobile.jpg",
-            "tablet": "./public/assets/image-brownie-tablet.jpg",
-            "desktop": "./public/assets/image-brownie-desktop.jpg"
+            "thumbnail": "./assets/image-brownie-thumbnail.jpg",
+            "mobile": "./assets/image-brownie-mobile.jpg",
+            "tablet": "./assets/image-brownie-tablet.jpg",
+            "desktop": "./assets/image-brownie-desktop.jpg"
         },
         "name": "Salted Caramel Brownie",
         "category": "Brownie",
@@ -89,10 +89,10 @@ export const data = [
      },
      {
         "image": {
-            "thumbnail": "./public/assets/image-panna-cotta-thumbnail.jpg",
-            "mobile": "./public/assets/image-panna-cotta-mobile.jpg",
-            "tablet": "./public/assets/image-panna-cotta-tablet.jpg",
-            "desktop": "./public/assets/image-panna-cotta-desktop.jpg"
+            "thumbnail": "./assets/image-panna-cotta-thumbnail.jpg",
+            "mobile": "./assets/image-panna-cotta-mobile.jpg",
+            "tablet": "./assets/image-panna-cotta-tablet.jpg",
+            "desktop": "./assets/image-panna-cotta-desktop.jpg"
         },
         "name": "Vanilla Panna Cotta",
         "category": "Panna Cotta",
